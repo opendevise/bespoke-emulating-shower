66dd6505c839274aeb03e24f84f00258ff9dd547
Dan Allen <dan.j.allen@gmail.com>
2016-02-03 04:27:57 -0700

    modernize gulpfile
    - update all gulp-related dependencies
    - call browserify using the recommended approach
    - update index.jade to use latest Jade syntax
    - add notes to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 'use strict';
 
 var pkg = require('./package.json'),
-  gulp = require('gulp'),
-  gutil = require('gulp-util'),
   autoprefixer = require('gulp-autoprefixer'),
-  browserify = require('gulp-browserify'),
+  browserify = require('browserify'),
+  buffer = require('vinyl-buffer'),
   chmod = require('gulp-chmod'),
   connect = require('gulp-connect'),
   csso = require('gulp-csso'),
   del = require('del'),
   exec = require('gulp-exec'),
   ghpages = require('gh-pages'),
+  gulp = require('gulp'),
+  gutil = require('gulp-util'),
   jade = require('gulp-jade'),
   path = require('path'),
   plumber = require('gulp-plumber'), // plumber prevents pipe breaking caused by errors thrown by plugins
   rename = require('gulp-rename'),
+  source = require('vinyl-source-stream'),
   stylus = require('gulp-stylus'),
   through = require('through'),
   tidy = require('tidy-html5').tidy_html5,
@@ -22,12 +24,13 @@ var pkg = require('./package.json'),
   isDist = process.argv.indexOf('deploy') >= 0;
 
 gulp.task('js', ['clean:js'], function() {
-  return gulp.src('src/scripts/main.js')
-    .pipe(isDist ? through() : plumber())
-    .pipe(browserify({ transform: ['debowerify'] }))
-    //.pipe(isDist ? uglify() : through())
+  // see https://wehavefaces.net/gulp-browserify-the-gulp-y-way-bb359b3f9623
+  return browserify('src/scripts/main.js').bundle()
+    // NOTE this error handler fills the role of plumber() when working with browserify
+    .on('error', function(e) { if (isDist) { throw e; } else { gutil.log(e.stack); this.emit("end"); } })
+    .pipe(source('build.js'))
+    .pipe(buffer())
     .pipe(uglify())
-    .pipe(rename('build.js'))
     .pipe(gulp.dest('dist/build'))
     .pipe(connect.reload());
 });
@@ -35,16 +38,16 @@ gulp.task('js', ['clean:js'], function() {
 gulp.task('jade-html', ['clean:jade-html'], function() {
   return gulp.src('src/index.jade')
     .pipe(isDist ? through() : plumber())
-    .pipe(jade({ pretty: true }))
+    .pipe(jade({ pretty: '  ' }))
     .pipe(rename('index-jade.html'))
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 });
 
-gulp.task('asciidoc-html', ['clean:asciidoc-html'], function(done) {
-  return gulp.src('')
+gulp.task('asciidoc-html', ['clean:asciidoc-html'], function() {
+  return gulp.src('src/index.adoc')
     .pipe(isDist ? through() : plumber())
-    // using stdin here would cause loss of context
+    // NOTE using stdin here would cause loss of context
     .pipe(exec('asciidoctor-bespoke -o - src/index.adoc', { pipeStdout: true }))
     .pipe(exec.reporter({ stdout: false }))
     .pipe(through(function(file) {
@@ -63,7 +66,7 @@ gulp.task('asciidoc-html', ['clean:asciidoc-html'], function(done) {
         'wrap': 0
       });
       html = html
-        // strip extra newlines inside <pre> tags (fixed in 5.1)
+        // strip extra newlines inside <pre> tags (fixed in tidy 5.1)
         .replace(new RegExp('<pre([^>]*)>\\n([\\s\\S]*?)\\n</pre>', 'g'), '<pre$1>$2</pre>\n')
         // strip extra endline after <script> start tag
         //.replace(new RegExp('>\\n</script>', 'g'), '></script>')
@@ -81,10 +84,7 @@ gulp.task('asciidoc-html', ['clean:asciidoc-html'], function(done) {
 gulp.task('css', ['clean:css'], function() {
   return gulp.src('src/styles/main.styl')
     .pipe(isDist ? through() : plumber())
-    .pipe(stylus({
-      'include css': true, // Allow CSS to be imported from node_modules
-      paths: ['./node_modules']
-    }))
+    .pipe(stylus({ 'include css': true, paths: ['./node_modules'] }))
     .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(isDist ? csso() : through())
     .pipe(rename('build.css'))
@@ -98,50 +98,40 @@ gulp.task('images', ['clean:images'], function() {
     .pipe(connect.reload());
 });
 
-gulp.task('clean', function(done) {
-  del('dist', done);
+gulp.task('clean', function() {
+  return del('dist');
 });
 
-gulp.task('clean:jade-html', function(done) {
-  del('dist/index-jade.html', done);
+gulp.task('clean:jade-html', function() {
+  return del('dist/index-jade.html');
 });
 
-gulp.task('clean:asciidoc-html', function(done) {
-  del('dist/index.html', done);
+gulp.task('clean:asciidoc-html', function() {
+  return del('dist/index.html');
 });
 
-gulp.task('clean:js', function(done) {
-  del('dist/build/build.js', done);
+gulp.task('clean:js', function() {
+  return del('dist/build/build.js');
 });
 
-gulp.task('clean:css', function(done) {
-  del('dist/build/build.css', done);
+gulp.task('clean:css', function() {
+  return del('dist/build/build.css');
 });
 
-gulp.task('clean:images', function(done) {
-  del('dist/images', done);
+gulp.task('clean:images', function() {
+  return del('dist/images');
 });
 
 gulp.task('connect', ['build'], function() {
-  connect.server({
-    root: 'dist',
-    port: 8000,
-    livereload: true
-  });
-});
-
-gulp.task('open', ['connect'], function (done) {
+  connect.server({ root: 'dist', port: 8000, livereload: true });
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/**/*.adoc', ['asciidoc-html']);
   gulp.watch('src/**/*.jade', ['jade-html']);
+  gulp.watch('src/scripts/**/*.js', ['js']);
   gulp.watch('src/styles/**/*.styl', ['css']);
   gulp.watch('src/images/**/*', ['images']);
-  gulp.watch([
-    'src/scripts/**/*.js',
-    'bespoke-theme-*/dist/*.js' // Allow themes to be developed in parallel
-  ], ['js']);
 });
 
 gulp.task('deploy', ['clean', 'build'], function(done) {
@@ -150,6 +140,6 @@ gulp.task('deploy', ['clean', 'build'], function(done) {
 
 gulp.task('build', ['js', 'asciidoc-html', 'jade-html', 'css', 'images']);
 
-gulp.task('serve', ['open', 'watch']);
+gulp.task('serve', ['connect', 'watch']);
 
 gulp.task('default', ['build']);
